998abaf5b40358541aeebc6021ee8923ea8d432c
Pritam Das <pritam.acc@gmail.com>
2021-06-29 15:16:31 +0000

    Added vnet module-1

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,6 @@
 module "network-pritam" {
   source  = "iacdev.wpp.cloud/WPP-Lab/network-pritam/azurerm"
   version = "3.0.1"
+  resource_group_name  = azurerm_resource_group.myresourcegroup.name
   # insert required variables here
 }
